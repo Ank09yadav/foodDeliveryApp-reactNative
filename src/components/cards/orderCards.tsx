11dcde52277cbbd6bb3b64a64cdb227d57ffc20a
Ank09yadav <ankury114112118@gmail.com>
2026-05-21 23:22:20 +0530

    add card

---
+++ src/components/cards/orderCards.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const orderCards = () => {
+  return (
+    <View>
+      <Text>orderCards</Text>
+    </View>
+  )
+}
+
+export default orderCards
+
+const styles = StyleSheet.create({})
