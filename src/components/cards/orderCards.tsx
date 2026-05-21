6e3ff33941d1c135a6adb27e32b66c29f07b24a4
Ank09yadav <ankury114112118@gmail.com>
2026-05-21 23:30:15 +0530

    add OrderCards component and integrate it into HomeScreen

--- src/components/cards/orderCards.tsx
+++ src/components/cards/orderCards.tsx
@@ -1,14 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const orderCards = () => {
+const OrderCards = () => {
   return (
     <View>
+      <Image source={ require('../../../assets/images/onboarding1.jpg')} style={{width:100, height:100 }}/>
       <Text>orderCards</Text>
     </View>
   )
 }
 
-export default orderCards
+export default OrderCards
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+})
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
+import OrderCards from '@/components/cards/orderCards';
+
+
 const HomeScreen = () => {
   return (
    <View style={styles.container}>
       <Text style={styles.text}>Welcome to the Home Screen! 🚀</Text>
+      <OrderCards/>
     </View>
+
   )
 }
 
